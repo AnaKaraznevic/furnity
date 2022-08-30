19dbabdfbcc23733429847210dd42ef7a34e6c3b
Ana Karaznevic <andrulenaite@gmail.com>
2022-08-30 14:09:07 +0300

    Initial commit

---
+++ src/main/java/com/furnity/furnity/FurnityApplication.java
@@ -0,0 +1,13 @@
+package com.furnity.furnity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurnityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurnityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/furnity/furnity/FurnityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furnity.furnity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurnityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
