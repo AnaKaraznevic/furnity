335e2034ae0fc08f06c3ae40d791c804455537bc
kasper108 <stanislovas.kasperiunas@gmail.com>
2022-09-06 00:20:16 +0300

    Improved

--- src/main/java/com/furnity/furnity/controller/CategoryController.java
+++ src/main/java/com/furnity/furnity/controller/CategoryController.java
@@ -3,14 +3,10 @@ package com.furnity.furnity.controller;
 import com.furnity.furnity.model.Category;
 import com.furnity.furnity.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
