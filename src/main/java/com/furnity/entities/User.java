cf5f7cefd4caf59a66774dfa8f4ae8887c135828
nakul chetiwal <chetiwalnakul7@gmail.com>
2022-09-05 03:11:54 -0400

    used @Data annotation instead of getter,setter,tostring()and constructor

--- src/main/java/com/furnity/entities/User.java
+++ src/main/java/com/furnity/entities/User.java
@@ -6,7 +6,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+
 @Entity
+@Data
 public class User {
     @Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -24,94 +27,6 @@ public class User {
 	private String postalcode;
 	private String created;
 	private String updated;
-	public int getUser_id() {
-		return user_id;
-	}
-	public void setUser_id(int user_id) {
-		this.user_id = user_id;
-	}
-	public String getFname() {
-		return fname;
-	}
-	public void setFname(String fname) {
-		this.fname = fname;
-	}
-	public String getLname() {
-		return lname;
-	}
-	public void setLname(String lname) {
-		this.lname = lname;
-	}
-	public String getPhone() {
-		return phone;
-	}
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPass() {
-		return pass;
-	}
-	public void setPass(String pass) {
-		this.pass = pass;
-	}
-	public String getGender() {
-		return gender;
-	}
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-	public String getStreet() {
-		return street;
-	}
-	public void setStreet(String street) {
-		this.street = street;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public String getCountry() {
-		return country;
-	}
-	public void setCountry(String country) {
-		this.country = country;
-	}
-	public String getPostalcode() {
-		return postalcode;
-	}
-	public void setPostalcode(String postalcode) {
-		this.postalcode = postalcode;
-	}
-	public String getCreated() {
-		return created;
-	}
-	public void setCreated(String created) {
-		this.created = created;
-	}
-	public String getUpdated() {
-		return updated;
-	}
-	public void setUpdated(String updated) {
-		this.updated = updated;
-	}
-	@Override
-	public String toString() {
-		return "User [user_id=" + user_id + ", fname=" + fname + ", lname=" + lname + ", phone=" + phone + ", email="
-				+ email + ", pass=" + pass + ", gender=" + gender + ", street=" + street + ", city=" + city
-				+ ", country=" + country + ", postalcode=" + postalcode + ", created=" + created + ", updated="
-				+ updated + "]";
-	}
-	
-	
-	
 	
 	
 }
