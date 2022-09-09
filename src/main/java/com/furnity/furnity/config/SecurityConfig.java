682eec79d6262915f8138e32938093161e331cf7
Stanislovas Kasperiūnas <stanislovaskasperiunas@gmail.com>
2022-09-09 19:58:41 +0300

    Main Branch - Code is working for Nakul

--- src/main/java/com/furnity/furnity/config/SecurityConfig.java
+++ src/main/java/com/furnity/furnity/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.furnity.furnity.config;
 
-import com.furnity.furnity.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
