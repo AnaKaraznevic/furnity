1ca4c9592379bc3f861b649d67929f50b77a4198
nakul chetiwal <chetiwalnakul7@gmail.com>
2022-09-10 15:23:30 -0400

    logout sorted

--- src/main/java/com/furnity/furnity/controller/UserController.java
+++ src/main/java/com/furnity/furnity/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.furnity.furnity.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -28,12 +33,11 @@ public class UserController {
 	@GetMapping({ "/", "/home" })
 	public String home(Model model, HttpSession session) {
 		// model.addAttribute("customers", customerservice.listAllCustomers());
+		
 		if (securityService.isAuthenticated()) {
-
 			User user = (User) session.getAttribute("isUserLoggedInData");
 			model.addAttribute("user", user);
 		}
-
 		return "home";
 	}
 
@@ -87,15 +91,15 @@ public class UserController {
 
 	}
 
-	@GetMapping(value = "/logout")
-	public String logout(HttpSession session) {
-
-		System.out.println("Redirecting to products.html page.");
-		session.setAttribute("isUserLoggedIn", false);
-		session.setAttribute("isUserLoggedInData", null);
-
-		return "home";
-	}
+	
+	@GetMapping(value="/logout")  
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {  
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();  
+        if (auth != null){      
+           new SecurityContextLogoutHandler().logout(request, response, auth);  
+        }  
+         return "redirect:/";  
+     }
 
 	@GetMapping(value = "/register")
 	public String register() {
